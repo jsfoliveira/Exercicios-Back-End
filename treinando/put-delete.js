19b821cef11d5814689437db95c9fc94af3ab5af
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-06-09 22:21:55 -0300

    treinando put e delete

---
+++ treinando/put-delete.js
@@ -0,0 +1,4 @@
+// PUT: atualiza
+// DELETE: deleta objetos.
+
+// Ao utilizar o .end() na callback da rota PUT /recipes/:id, nada foi retornado, apenas o status 204, que indica que a requisição foi concluída com sucesso.
